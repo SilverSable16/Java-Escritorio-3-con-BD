f6fd0037545d1db4dbb5bd31d41f12f610f0806e
unknown <jponcet@miumg.edu.gt>
2022-09-03 14:28:45 -0600

    Tarea

---
+++ src/modelo/Empleado.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author JPonce
+ */
+public class Empleado extends Persona {
+    private String codigo,puesto;
+
+    public Empleado(){}
+    public Empleado(String codigo, String puesto, String nombres, String apellidos, String direccion, String telefono, String fecha_nacimiento) {
+        super(nombres, apellidos, direccion, telefono, fecha_nacimiento);
+        this.codigo = codigo;
+        this.puesto = puesto;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getPuesto() {
+        return puesto;
+    }
+
+    public void setPuesto(String puesto) {
+        this.puesto = puesto;
+    }
+    
+    @Override
+    public void agregar(){
+      System.out.println("Codigo: " + getCodigo());
+      System.out.println("Puesto: " + getPuesto());
+      System.out.println("Nombres: " +  getNombres() );
+      System.out.println("Apellidos: " + getApellidos() );
+      System.out.println("Direccion: " + getDireccion() );
+      System.out.println("Telefono: " + getTelefono() );
+      System.out.println("Fecha Nacimiento: " + getFecha_nacimiento());
+      System.out.println("__________________________________________");
+    }
+}
